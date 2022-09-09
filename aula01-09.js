7d6c6cc5c1d79475eb305d113a81f5c937572afd
PedroMittmann <pedromittmann11@gmail.com>
2022-09-08 21:00:37 -0400

    aula08-09

--- aula01-09.js
+++ aula01-09.js
@@ -1,12 +1,45 @@
 //1. Crie um código que consiga imprimir os tipos de 3 variáveis diferentes;
 var numero = 2;
+const letra = "pedro";
+let y = "name";
+
+console.log(numero, letra, y);
+
 //2. Crie um código capaz de calcular a raiz quadrada de um número;
+let RaizQuadrada,b;
+ b = 4;
+RaizQuadrada=Math.sqrt(b);
+
+console.log(RaizQuadrada);
 
 //3. Crie um código capaz de calcular a potência 2 de um número;
+let Potencia,c;
+c = 4;
+Potencia = Math.pow(c,2);
+console.log(Potencia);
 
 //4. Crie um código que calcule a hipotenusa através do teorema de pitágoras;
+let h,c1,c2;
+c1=2;
+c2=4;
+c1=Math.pow(c1,2);
+c2=Math.pow(c2,2);
+h=c1+c2
+console.log("Hipotenusa é igual: "+h);
 
 //5. Crie um código capaz de calcular a fórmula de bhaskara;
+
+    let a,b,c;
+    let Delta,x1,x2;
+    a=2;
+    b=3;
+    c=6;
+
+    delta=Math.pow(b,2)-4*a*c;
+    x1=(-b+delta)/2*a;
+    x2=(-b-delta)/2*a;
+
+    console.log(x1,x2);
 
 //6. Crie um objeto json com um ítem do tipo array, e acesse a segunda posição;
 
