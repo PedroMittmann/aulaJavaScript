91568b6cc13565fb7e3dff5ee3cb57792b837456
PedroMittmann <pedromittmann11@gmail.com>
2022-09-08 20:11:42 -0400

    first commit

---
+++ aula01-09.js
@@ -0,0 +1,26 @@
+/*
+let a = 30;
+let b = 2;
+let c = -2;
+
+let delta = Math.pow(b, 2) -4 *a *c;
+
+let x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
+let x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
+console.log(Math.cbrt(100));
+console.log(x1,x2);
+ 
+if (a<2){
+    console.log("1");
+
+}else {
+    console.log("2");
+}
+*/
+ senha = 123451454;
+
+if (senha = 1234){
+    console.log("ACESSO PERMITIDO");
+}else{
+    console.log("ACESSO NEGADO");
+}
